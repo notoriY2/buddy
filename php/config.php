331d4fd7eff15ae2f58c418db33f40b319056050
NotoriousY2 <notori.y2@gmail.com>
2026-01-08 11:38:14 +0200

    Confirm app entry index.php

--- php/config.php
+++ php/config.php
@@ -1,11 +1,13 @@
 <?php
-  $hostname = "127.0.0.1";
-  $username = "root";
-  $password = "";
-  $dbname = "buddy";
+$hostname = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$dbname   = getenv("DB_NAME");
+$port     = getenv("DB_PORT") ?: 3306;
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
-  if(!$conn){
-    echo "Database connection error".mysqli_connect_error();
-  }
+$conn = mysqli_connect($hostname, $username, $password, $dbname, $port);
+
+if (!$conn) {
+    die("Database connection error");
+}
 ?>
